a1e156ba7f1a96e13e01f88b84c81d646a05b97c
Plamen Yovchev <plamenkooo@gmail.com>
2020-09-30 16:43:30 +0300

    Add components

---
+++ pages/speakers.js
@@ -0,0 +1,19 @@
+import Header from "../src/components/Header/Header";
+import Menu from "../src/components/Menu/Menu";
+import Searchbar from "../src/components/Searchbar/Searchbar";
+import Speakers from "../src/components/Speakers/Speakers";
+import Footer from "../src/components/Footer/Footer";
+
+const Page = () => {
+  return (
+    <>
+      <Header />
+      <Menu />
+      <Searchbar />
+      <Speakers />
+      <Footer />
+    </>
+  );
+};
+
+export default Page;
