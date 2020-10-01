22b5bbe31f59f13674a278c78497339c3f3fc05c
Plamen Yovchev <plamenkooo@gmail.com>
2020-10-01 17:44:37 +0300

    Wrap Speakers component with Layout

--- pages/speakers.js
+++ pages/speakers.js
@@ -1,17 +1,12 @@
-import Header from "../src/components/Header/Header";
-import Menu from "../src/components/Menu/Menu";
-import Searchbar from "../src/components/Searchbar/Searchbar";
+import Layout from "../src/components/Layout/Layout";
 import Speakers from "../src/components/Speakers/Speakers";
-import Footer from "../src/components/Footer/Footer";
 
 const Page = () => {
   return (
     <>
-      <Header />
-      <Menu />
-      <Searchbar />
-      <Speakers />
-      <Footer />
+      <Layout>
+        <Speakers />
+      </Layout>
     </>
   );
 };
