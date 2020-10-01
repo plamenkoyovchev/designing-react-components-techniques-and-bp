57bc88ca2e497d6052228707b2aca5d7f7de17f9
Plamen Yovchev <plamenkooo@gmail.com>
2020-10-01 17:44:22 +0300

    Add content  to home page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
+import Layout from "../src/components/Layout/Layout";
+
 const Page = () => {
   return (
-    <div>
-      <h1>Hello from me!</h1>
-    </div>
+    <Layout>
+      <div className="text-gray-600">
+        <div className="m-6">
+          <p>
+            Silicon Valley Code Camp is a two - day technical conference in
+            Silicon Valley where developers learn from developers focusing on
+            open source, the latest enterprise - focused technologies, software
+            branding, legal issues around software as well as other topics
+            developers are interested in hearing about such as career building
+            and more.topics developers are interested in hearing about.
+          </p>
+        </div>
+      </div>
+    </Layout>
   );
 };
 
