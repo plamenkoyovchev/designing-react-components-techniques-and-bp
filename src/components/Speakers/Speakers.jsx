94f47001ed2de9d9047c6736f2b9b5b8d5b9f89e
Plamen Yovchev <plamenkooo@gmail.com>
2020-10-01 17:44:00 +0300

    Add jsx to components

--- src/components/Speakers/Speakers.jsx
+++ src/components/Speakers/Speakers.jsx
@@ -4,10 +4,35 @@ import withData from "../HOCs/withData";
 const Speakers = ({ speakers }) => {
   return (
     <div>
-      {speakers &&
-        speakers.map(({ imageSrc, name }) => (
-          <img src={`images/${imageSrc}.png`} title={name} key={imageSrc} />
+      <div className="mb-6 ">
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="username"
+          type="text"
+          placeholder="Search by name"
+        />
+      </div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-12">
+        {speakers.map(({ id, firstName, lastName, bio, isFavorite }) => (
+          <div className="rounded overflow-hidden shadow-lg p-6" key={id}>
+            <div className="grid grid-cols-4 mb-6">
+              <div className="font-bold text-lg col-span-3">{`${firstName} ${lastName}`}</div>
+              <div className="flex justify-end">
+                <div
+                  className={isFavorite ? "heartredbutton" : "heartdarkbutton"}
+                ></div>
+              </div>
+            </div>
+            <div className="mb-6">
+              <img
+                src={`/speakers/speaker-${id}.jpg`}
+                alt={`${firstName} ${lastName}`}
+              />
+            </div>
+            <div className="text-gray-600">{bio.substr(0, 70) + "..."}</div>
+          </div>
         ))}
+      </div>
     </div>
   );
 };
