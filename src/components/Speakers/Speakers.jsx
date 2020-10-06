2843ed4a0b8d39add073ff0db88ec8e0d5d591d8
plamenyovchev <plamenkooo@gmail.com>
2020-10-06 21:23:36 +0300

    Use Searchbar & Speaker components

--- src/components/Speakers/Speakers.jsx
+++ src/components/Speakers/Speakers.jsx
@@ -1,37 +1,15 @@
 import React from "react";
 import withData from "../HOCs/withData";
+import Searchbar from "../Searchbar/Searchbar";
+import Speaker from "../Speaker/Speaker";
 
 const Speakers = ({ speakers }) => {
   return (
     <div>
-      <div className="mb-6 ">
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="username"
-          type="text"
-          placeholder="Search by name"
-        />
-      </div>
+      <Searchbar />
       <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-12">
-        {speakers.map(({ id, firstName, lastName, bio, isFavorite }) => (
-          <div className="rounded overflow-hidden shadow-lg p-6" key={id}>
-            <div className="grid grid-cols-4 mb-6">
-              <div className="font-bold text-lg col-span-3">{`${firstName} ${lastName}`}</div>
-              <div className="flex justify-end">
-                <div
-                  className={isFavorite ? "heartredbutton" : "heartdarkbutton"}
-                ></div>
-              </div>
-            </div>
-            <div className="mb-6">
-              <img
-                src={`/speakers/speaker-${id}.jpg`}
-                alt={`${firstName} ${lastName}`}
-              />
-            </div>
-            <div className="text-gray-600">{bio.substr(0, 70) + "..."}</div>
-          </div>
-        ))}
+        {speakers &&
+          speakers.map((speaker) => <Speaker key={speaker.id} {...speaker} />)}
       </div>
     </div>
   );
