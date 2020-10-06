ef75b6726b8911ff08dbaca07570429953875e1c
plamenyovchev <plamenkooo@gmail.com>
2020-10-06 21:44:12 +0300

    Add Speakers search functionality

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-const Searchbar = () => (
+const Searchbar = ({ speakersQuery, setSpeakersQuery }) => (
   <div className="mb-6 ">
     <input
       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
       id="username"
       type="text"
       placeholder="Search by name"
+      value={speakersQuery}
+      onChange={(e) => setSpeakersQuery(e.target.value)}
     />
   </div>
 );
--- src/components/Speakers/Speakers.jsx
+++ src/components/Speakers/Speakers.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import withData from "../HOCs/withData";
 import Searchbar from "../Searchbar/Searchbar";
 import Speaker from "../Speaker/Speaker";
 
 const Speakers = ({ speakers }) => {
+  const [speakersQuery, setSpeakersQuery] = useState("");
+
+  const filterSpeaker = ({ firstName, lastName }) => {
+    return (
+      firstName.toLowerCase().includes(speakersQuery.toLowerCase()) ||
+      lastName.toLowerCase().includes(speakersQuery.toLowerCase())
+    );
+  };
+
   return (
     <div>
-      <Searchbar />
+      <Searchbar
+        speakersQuery={speakersQuery}
+        setSpeakersQuery={setSpeakersQuery}
+      />
       <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-12">
         {speakers &&
-          speakers.map((speaker) => <Speaker key={speaker.id} {...speaker} />)}
+          speakers
+            .filter(filterSpeaker)
+            .map((speaker) => <Speaker key={speaker.id} {...speaker} />)}
       </div>
     </div>
   );
