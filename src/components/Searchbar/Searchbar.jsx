6d4c51a6cbc00f1c088ce05c4b250b4d263df0d7
plamenyovchev <plamenkooo@gmail.com>
2020-10-06 21:23:10 +0300

    Introduce Searchbar component

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-const Searchbar = () => <img src="images/searchbar.gif" />;
+const Searchbar = () => (
+  <div className="mb-6 ">
+    <input
+      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      id="username"
+      type="text"
+      placeholder="Search by name"
+    />
+  </div>
+);
 
 export default Searchbar;
