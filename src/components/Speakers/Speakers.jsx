98809da552b19666360d3736925c362acd0e4771
plamenyovchev <plamenkooo@gmail.com>
2020-10-07 20:49:15 +0300

    refactor Speakers to update state with useReducer

--- src/components/Speakers/Speakers.jsx
+++ src/components/Speakers/Speakers.jsx
@@ -1,18 +1,73 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 
 import axios from "axios";
 import withData from "../HOCs/withData";
 import Searchbar from "../Searchbar/Searchbar";
 import Speaker from "../Speaker/Speaker";
 
+const actionTypes = {
+  FETCH_SPEAKERS_START: "FETCH_SPEAKERS_START",
+  FETCH_SPEAKERS_SUCCESS: "FETCH_SPEAKERS_SUCCESS",
+  FETCH_SPEAKERS_FAILED: "FETCH_SPEAKERS_FAILED",
+  UPDATE_SPEAKERS_START: "UPDATE_SPEAKERS_START",
+  UPDATE_SPEAKERS_SUCCESS: "UPDATE_SPEAKERS_SUCCESS",
+  UPDATE_SPEAKERS_FAILED: "UPDATE_SPEAKERS_FAILED",
+};
+
+const initialState = {
+  speakers: [],
+  loading: false,
+  error: "",
+};
+
+const speakersReducer = (state, action) => {
+  switch (action.type) {
+    case actionTypes.FETCH_SPEAKERS_START:
+      return {
+        ...state,
+        loading: true,
+        error: "",
+      };
+    case actionTypes.FETCH_SPEAKERS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        speakers: action.payload,
+        error: "",
+      };
+    case actionTypes.FETCH_SPEAKERS_FAILED:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
 const Speakers = () => {
-  const [speakers, setSpeakers] = useState([]);
+  const [{ speakers, loading, error }, dispatch] = useReducer(
+    speakersReducer,
+    initialState
+  );
   const [speakersQuery, setSpeakersQuery] = useState("");
 
   useEffect(() => {
     const fetchSpeakers = async () => {
-      const response = await axios.get("http://localhost:3004/speakers");
-      setSpeakers(response.data);
+      try {
+        dispatch({ type: actionTypes.FETCH_SPEAKERS_START });
+        const response = await axios.get("http://localhost:3004/speakers");
+        dispatch({
+          type: actionTypes.FETCH_SPEAKERS_SUCCESS,
+          payload: response.data,
+        });
+      } catch {
+        dispatch({
+          type: actionTypes.FETCH_SPEAKERS_FAILED,
+          payload: "Unable to get speakers",
+        });
+      }
     };
 
     fetchSpeakers();
@@ -54,6 +109,8 @@ const Speakers = () => {
         speakersQuery={speakersQuery}
         setSpeakersQuery={setSpeakersQuery}
       />
+      {loading && <h3>Loading...</h3>}
+      {error && <h3>{error}</h3>}
       <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-12">
         {speakers &&
           speakers
